f8856644cbcc4bda17ff5c07368aa6b27e2761a9
Shidesin <shidesin@gmail.com>
2020-11-09 15:00:39 +0300

    Create component ProfileSatus with Hook useEffect. LESSON 85

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 type PropsType = {
     status: string
@@ -7,6 +7,10 @@ type PropsType = {
 
 
 const ProfileStatusWithHooks = (props: PropsType) => {
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const [state, setState] = useState<boolean>(false)
     const [status, setStatus] = useState<string>(props.status)
